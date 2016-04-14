77cd1cb5b5afa2c0018e4f46ba6d65d988106439
Keith Prifte <kprift@gmail.com>
2016-04-14 11:30:18 -0400

    adds index and show views

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-var express = require("express");
-var app = express()
+
+var express      = require("express");
+var mongoose     = require("./db/connection.js");
+var hbs          = require("express-handlebars");
+var parser       = require("body-parser");
+var app          = express()
+
+var Primaries    = mongoose.model("Primaries")
+
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname:       ".hbs",
+  partialsDir:   "views/",
+  layoutsDir:    "views/",
+  defaultLayout: "layout-main"
+}));
+
+app.use(parser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-  res.send("hello")
+  Primaries.find().then(function(primaries){
+    res.render("primaries-index", {
+      primaries
+    });
+  });
+});
+
+app.get("/:state", function(req, res){
+  Primaries.findOne({state: req.params.state}).then(function(primary){
+    res.render("primaries-show", {
+      primary
+    });
+  });
 });
 
 app.listen(3001, function(){
